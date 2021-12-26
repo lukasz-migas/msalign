206055809d2cdaae889b7615d9f64af7690c5d0c
Lukasz Migas <lukas.migas@yahoo.com>
2021-12-26 18:18:29 +0100

    Update __init__.py

--- msalign/__init__.py
+++ msalign/__init__.py
@@ -3,7 +3,10 @@ from typing import List
 
 import numpy as np
 
-from ._version import __version__  # noqa: F401
+try:
+    from ._version import version as __version__  # noqa: F401
+except ImportError:
+    __version__ = "unknown"
 from .align import Aligner
 
 __all__ = ["msalign", "Aligner"]
